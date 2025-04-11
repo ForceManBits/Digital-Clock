2d8616e61d4a4be41c68539331e39b0c2d8768f7
Guilherme Reis <89349692+ForceManBits@users.noreply.github.com>
2025-04-11 09:24:17 -0300

    initial upload.

---
+++ main.js
@@ -0,0 +1,25 @@
+function updateClock(){
+    const now = new Date();
+
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+hours = hours < 10 ? "0" + hours : hours;
+minutes = minutes < 10 ? "0" + minutes : minutes;
+seconds = seconds < 10 ? "0" + seconds : seconds;
+
+const formattedText = `${hours} : ${minutes}: ${seconds}`;
+
+document.getElementById("clock").textContent = formattedText;
+
+setTimeout(updateClock, 1000);
+
+}
+
+window.onload = updateClock;
+
+
+
+
